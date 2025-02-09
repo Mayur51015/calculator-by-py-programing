6340ba083f40c21423f678eaf148ce3e97247797
Mayur51015 <Mayur2006khandare@gmail.com>
2025-02-09 23:12:50 +0530

    index.py

---
+++ index.py
@@ -0,0 +1,14 @@
+a=10
+b=5
+ans1=a+b
+print("Addition of",a,"and",b,"is",ans1)
+ans2=a-b
+print("Subtraction of",a,"and",b,"is",ans2)
+ans3=a*b
+print("multiplication of",a,"and",b,"is",ans3)
+ans4=a/b
+print("division of",a,"and",b,"is",ans4)
+ans5=a%b
+print("modulus of",a,"and",b,"is",ans5)
+ans6=a//b
+print("floor division of",a,"and",b,"is",ans6)
